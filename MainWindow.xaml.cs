39b3dcc3351bb01fd9189d04ef3532f255b5b828
ViktorK2910 <90631273+ViktorK2910@users.noreply.github.com>
2024-06-22 15:24:32 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Users
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Reg_Click(object sender, RoutedEventArgs e)
+        {
+            string login = textBoxLogin.Text.Trim();
+            string pass = passBox.Password.Trim();
+            string pass2 = passBox_2.Password.Trim();
+            string email = textBoxEmail.Text.Trim().ToLower();
+
+            if (login.Length < 5)
+            {
+                textBoxLogin.ToolTip = "Введите от 5 символов!";
+                textBoxLogin.Background = Brushes.DarkRed;
+            }
+            else if (pass.Length < 5)
+            {
+                passBox.ToolTip = "Введите от 5 символов!";
+                passBox.Background = Brushes.DarkRed;
+            }
+            else if (pass != pass2)
+            {
+                passBox_2.ToolTip = "Пароли не совпадают!";
+                passBox_2.Background = Brushes.DarkRed;
+            }
+            else if (!email.Contains("@") || !email.Contains("."))
+            {
+                textBoxEmail.ToolTip = "Пароли не совпадают!";
+                textBoxEmail.Background = Brushes.DarkRed;
+            }
+            else
+            {
+                textBoxLogin.ToolTip = "";
+                textBoxLogin.Background = Brushes.Transparent;
+                passBox.ToolTip = "";
+                passBox.Background = Brushes.Transparent;
+                passBox_2.ToolTip = "";
+                passBox_2.Background = Brushes.Transparent;
+                textBoxEmail.ToolTip = "";
+                textBoxEmail.Background = Brushes.Transparent;
+
+                MessageBox.Show("Всё хорошо");
+            }
+        }
+    }
+}
